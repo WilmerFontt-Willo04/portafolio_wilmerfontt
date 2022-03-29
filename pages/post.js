aed29f02604d9263ef764d90d03246526884130d
WilmerFontt-willo04 <wfonttbrito@gmail.com>
2022-03-29 12:24:36 -0400

    post

--- pages/post.js
+++ pages/post.js
@@ -1,35 +1,35 @@
-import Layout from "../components/Layout";
-import Link from "next/link";
-import Image from "next/image";
+// import Layout from "../components/Layout";
+// import Link from "next/link";
+// import Image from "next/image";
 
-import { useRouter } from "next/router";
-import { posts } from "../profile";
+// import { useRouter } from "next/router";
+// import { posts } from "../profile";
 
-const Post = () => {
-  const router = useRouter();
+// const Post = () => {
+//   const router = useRouter();
 
-  const currentPost = posts.filter(
-    (post) => post.title === router.query.title
-  )[0];
+//   const currentPost = posts.filter(
+//     (post) => post.title === router.query.title
+//   )[0];
 
-  return (
-    <Layout title={router.query.title} footer={false}>
-      <div className="text-center">
-        <Image
-          src="/vercel.svg"
-          alt="avatarpost"
-          style={{ width: "50%" }}
-          className="img-fluid"
-          width={600}
-          height={600}
-        />
-        <p className="p-4">{currentPost.content}</p>
-        <Link href="/blog" passHref>
-          <button className="btn btn-dark">Return</button>
-        </Link>
-      </div>
-    </Layout>
-  );
-};
+//   return (
+//     <Layout title={router.query.title} footer={false}>
+//       <div className="text-center">
+//         <Image
+//           src="/vercel.svg"
+//           alt="avatarpost"
+//           style={{ width: "50%" }}
+//           className="img-fluid"
+//           width={600}
+//           height={600}
+//         />
+//         <p className="p-4">{currentPost.content}</p>
+//         <Link href="/blog" passHref>
+//           <button className="btn btn-dark">Return</button>
+//         </Link>
+//       </div>
+//     </Layout>
+//   );
+// };
 
-export default Post;
+// export default Post;
